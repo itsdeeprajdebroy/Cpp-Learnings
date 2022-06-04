ba02fd4bfd9dcbc76b73d533f39db15a3c0c5555
Deepraj Debroy <itsdeeprajdebroy@gmail.com>
2022-06-04 08:47:50 +0530

    Codes Added

---
+++ Leetcode_Day_wise/Ques_4_day_3.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+//Approach 1
+// void sort012(int arr[], int n)
+// {
+//     int count0 = 0, count1 = 0, index = 0;
+//     while(index != n)
+//     {
+//         if(arr[index] == 0)
+//         {
+//             count0++;
+//         }
+//         else if(arr[index] == 1)
+//         {
+//             count1++;
+//         }
+//         index++;
+//     }
+//     int var = 0;
+//     index = 0;
+//     while(index != n)
+//     {
+//         switch (var)
+//         {
+//           case 0: arr[index] = 0;
+//                   count0--;
+//                   if(count0 == 0)
+//                   {
+//                       var = 1;
+//                   }
+//                   break;
+//           case 1: arr[index] = 1;
+//                   count1--;
+//                   if(count1 == 0)
+//                   {
+//                       var = 2;
+//                   }
+//                   break;
+//           case 2: arr[index] = 2;
+//                   break;
+//         }
+//         index++;
+//     }
+// }
+
+//Approach 2(Dutch National Flag Algo)--> One pass Solution
+void sort012UsingAlgo(int arr[] , int size)
+{
+  int low = 0, mid = 0, high = size - 1;
+  while(mid <= high)
+  {
+    switch(arr[mid])
+    {
+      case 0: swap(arr[mid] , arr[low]);
+              mid++;
+              low++;
+              break;
+
+      case 1: mid++;
+              break;
+
+      case 2: swap(arr[mid] , arr[high]);
+              high--;
+              break;
+    }
+  }
+}
+
+int main()
+{
+  int arr[] = {2, 0, 1, 1, 1};
+  sort012UsingAlgo(arr, 5);
+  for(int i = 0; i < 5; i++)
+  {
+    cout<<arr[i] <<" ";
+  }
+}
---
+++ Leetcode_Day_wise/Ques_5_day_3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& arr) {
+        int n = arr.size() - 1, low = 0, mid = 0, high = n;
+        while(mid <= high)
+        {
+            switch(arr[mid] % 2)
+            {
+                case 0: mid++;
+                        low++;
+                        break;
+
+                case 1: swap(arr[mid] , arr[high]);
+                        high--;
+                        break;
+            }
+        }
+        return arr;
+    }
+};
